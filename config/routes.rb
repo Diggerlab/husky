bac2e7a36a1892f4648b80577a6b33895ffa004f
Mike Chen <chagel@gmail.com>
2012-12-10 18:24:26 +0800

    rename pages

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ Husky::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  resources :applies
+  root to: 'welcome#index'
 end
