cdad40d4c565b0b586111f5cf945706b0b3ed991
Mike Chen <chagel@gmail.com>
2012-12-11 16:28:30 +0800

    add active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Husky::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :applies
   resources :welcome do 
   	collection do 
