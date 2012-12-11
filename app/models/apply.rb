7d4afb100bb71b0028e5c8f2c23f2fb387e0e339
Mike Chen <chagel@gmail.com>
2012-12-11 16:21:46 +0800

    add applies controller

---
+++ app/models/apply.rb
@@ -0,0 +1,5 @@
+class Apply < ActiveRecord::Base
+  attr_accessible :email
+  validates_uniqueness_of :email
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+end
