4a23ae147418fef34485e7bb3c38a5cc23a52dab
Mike Chen <chagel@gmail.com>
2013-05-02 10:53:38 +0800

    update web host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :rails_env, "production" 
 
-role :web, "115.239.196.124"                          # Your HTTP server, Apache/etc
-role :app, "115.239.196.124"                          # This may be the same as your `Web` server
-role :db,  "115.239.196.124", :primary => true        # This is where Rails migrations will run
+role :web, "www.chichiapp.com"                          # Your HTTP server, Apache/etc
+role :app, "www.chichiapp.com"                          # This may be the same as your `Web` server
+# role :db,  "", :primary => true        # This is where Rails migrations will run
 
 set :branch, "master"
 
